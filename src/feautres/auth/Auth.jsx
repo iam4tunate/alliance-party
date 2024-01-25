6f6c4faaac60efcd6ce1045d6ffb5a8a146690ae
Fortunate ogodu <58645821+iam4tunate@users.noreply.github.com>
2024-01-25 12:26:41 +0100

    created the landing page, register page

--- src/feautres/auth/Auth.jsx
+++ src/feautres/auth/Auth.jsx
@@ -5,14 +5,14 @@ import LOGO from "../../assets/images/logo.png";
 const Auth = () => {
   return (
     <div className="w-full">
-      <div className="relative w-1/2">
+      <div className="relative w-1/2 max-lg:w-full">
         <Outlet />
       </div>
       <div
         style={{
           backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,1)),url(${BG})`,
         }}
-        className="bg-cover bg-center bg-no-repeat min-h-screen flex items-center justify-center text-white text-center py-12 px-8 fixed top-0 right-0 bottom-0 w-1/2"
+        className="max-lg:hidden bg-cover bg-center bg-no-repeat min-h-screen flex items-center justify-center text-white text-center py-12 px-8 fixed top-0 right-0 bottom-0 w-1/2"
       >
         <div className="flex flex-col items-center">
           <img src={LOGO} alt="" className="w-[15rem] h-[10rem] object-cover" />
--- src/feautres/auth/Register.jsx
+++ src/feautres/auth/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BsArrowRight } from "react-icons/bs";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 import { MdVerified } from "react-icons/md";
 import { Link } from "react-router-dom";
 
@@ -9,8 +9,8 @@ const Register = () => {
   return (
     <div className="relative text-base">
       {isRegistered && (
-        <div className="w-1/2 h-screen px-8 py-12 fixed top-0 left-0 bottom-0 bg-[rgba(0,0,0,0.8)] flex items-center">
-          <div className="flex flex-col text-center items-center gap-y-2 w-[80%] mx-auto bg-white rounded-md py-6 px-6">
+        <div className="w-1/2 max-lg:w-full h-screen padX py-12 fixed top-0 left-0 bottom-0 bg-[rgba(0,0,0,0.8)] flex items-center">
+          <div className="flex flex-col text-center items-center gap-y-2 w-[60%] lg:w-[80%] max-sm:w-full mx-auto bg-white rounded-md py-6 px-6">
             <MdVerified className="text-7xl text-primary" />
             <span className="text-xl font-Heebo500">
               Registration Successfull
@@ -19,18 +19,24 @@ const Register = () => {
               A text message will be sent to your phone number and an email to
               rmail inbox
             </span>
+            <Link to="/">
+              <div className="text-secondary pt-2">Go to homepage</div>
+            </Link>
           </div>
         </div>
       )}
       <div className="text-black px-8 py-12">
-        <div className="pb-10 flex items-center justify-between">
-          <span className="text-lg font-poppinsSemibold">
+        <div className="font-DMSefif text-2xl font-medium pb-2">
+          ALLIANCE POLITICAL PARTY REGISTRATION FORM
+        </div>
+        <div className="pb-10 flex max-sm:flex-col text-left items-center max-sm:items-start justify-between gap-y-2">
+          <span className="text-lg max-sm:text-base font-poppinsSemibold font-Heebo500">
             Register to become a verified member
           </span>
           <Link to="/">
-            <span className="text-sm text-secondary font-Heebo500 flex items-center gap-x-2">
-              <span>Go to Homepage</span>
-              <BsArrowRight />
+            <span className="text-base text-secondary font-Heebo500 flex items-center gap-x-1">
+              <BsArrowLeft />
+              <span>Go Back</span>
             </span>
           </Link>
           {/* <span className="block text-sm pt-1">
@@ -42,7 +48,7 @@ const Register = () => {
           member of this great party
         </span> */}
         </div>
-        <form className="grid grid-cols-2 gap-x-6 gap-y-8">
+        <form className="grid grid-cols-2 max-lg:grid-cols-1 gap-x-6 gap-y-8">
           <div className="flex flex-col">
             <label htmlFor="" className="label">
               Surname
@@ -166,7 +172,7 @@ const Register = () => {
           </div>
           <div
             onClick={() => setRegistered(true)}
-            className="bg-primary text-white py-3 px-10 col-start-1 col-end-3 w-fit justify-self-center rounded-md cursor-pointer"
+            className="bg-primary text-white py-3 px-10 col-start-1 col-end-3 max-lg:col-end-2 w-fit justify-self-center rounded-md cursor-pointer"
           >
             Register
           </div>
