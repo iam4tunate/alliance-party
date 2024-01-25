a275a166452b6f53960bb7656111fbdf3f5978db
Fortunate ogodu <58645821+iam4tunate@users.noreply.github.com>
2024-01-25 17:24:43 +0100

    general updating of content

--- src/ui/Commitment.jsx
+++ src/ui/Commitment.jsx
@@ -4,17 +4,17 @@ import { IoFlag } from "react-icons/io5";
 const Commitment = () => {
   return (
     <div className="pt-48 max-lg:pt-20 pb-20 max-sm:pt-16 max-sm:pb-16 maxW padX grid grid-cols-2 max-lg:grid-cols-1 gap-x-12 gap-y-8">
-      <div className="col-start-1 col-end-2">
+      <div className="col-start-1 col-end-2 space-y-3">
         <div className="bg-primary text-white col-start-2 col-end-3 max-lg:col-start-1 max-lg:col-end-2 py-6 px-4 max-sm:px-2 h-fit">
           <span className="font-DMSefif text-3xl">Mission</span>
           {MISSIONS.map((mission) => (
             <div
               key={mission.id}
-              className="pt-6 before:w-2 before:h-2 before:rounded-full before:bg-secondary before:inline-block before:mt-2 gap-x-3 grid grid-cols-[min-content_1fr]"
+              className="pt-6 before:w-2 before:h-2 before:rounded-full before:bg-white before:bg-opacity-80 before:inline-block before:mt-2 gap-x-3 grid grid-cols-[min-content_1fr]"
             >
               <div className="">
                 <div className="text-lg font-Heebo500">{mission.title}</div>
-                <p className="font-Heebo300 text-base opacity-90 leading-normal">
+                <p className="font-Heebo300 text-lg max-sm:text-base opacity-90 leading-normal">
                   {mission.text}
                 </p>
               </div>
@@ -30,7 +30,7 @@ const Commitment = () => {
             >
               <div className="">
                 <div className="text-lg font-Heebo500">{vision.title}</div>
-                <p className="font-Heebo400 text-base text-primary opacity- leading-normal">
+                <p className="font-Heebo400 text-lg max-sm:text-base text-primary opacity- leading-normal">
                   {vision.text}
                 </p>
               </div>
@@ -38,14 +38,14 @@ const Commitment = () => {
           ))}
         </div>
       </div>
-      <div className="">
-        <span className="bg-gray flex items-center gap-x-2 rounded-md py-2 px-2 w-fit text-base font-Heebo500">
+      <div className="max-lg:row-start-1 max-lg:row-end-2">
+        {/* <span className="bg-gray flex items-center gap-x-2 rounded-md py-2 px-2 w-fit text-base font-Heebo500">
           <IoFlag className="text-secondary" />
           <span>Our Commitment</span>
-        </span>
+        </span> */}
         <div className="py-5 px-4 max-sm:px-2">
           <span className="font-DMSefif text-primary text-3xl">About Us</span>
-          <div className="pt-3 leading-normal text-secondary">
+          <div className="pt-3 max-sm:text-lg leading-normal text-secondary">
             <p className="pb-2">
               <span className="font-Heebo500">The Kalesanwa Group</span> is a
               socio-economic and political strategy development organization,
