4a3a59e49590813b1ef7fcbe582b607221f9306d
Fortunate ogodu <58645821+iam4tunate@users.noreply.github.com>
2024-02-02 12:50:35 +0100

    removed the newsfeedsin the hero section into a component of its own

--- src/ui/Hero.jsx
+++ src/ui/Hero.jsx
@@ -1,47 +1,20 @@
-import { useEffect, useRef, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Autoplay } from "swiper/modules";
+import { useRef, useState } from "react";
+
 import { Link } from "react-router-dom";
-import { newsFeeds } from "../data";
-import "swiper/css";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-import BG from "../assets/images/heroBg.jpg";
-import axios from "axios";
+
+import NewsFeeds from "./NewsFeeds";
 
 const Hero = () => {
   // eslint-disable-next-line no-unused-vars
   const [_, setInit] = useState();
-  const [isBegin, setIsBegin] = useState(true);
-  const [isEnd, setIsEnd] = useState(false);
+  const [isBegin, setBegin] = useState(true);
+  const [isEnd, setEnd] = useState(false);
   const prevRef = useRef(null);
   const nextRef = useRef(null);
 
-  const [articles, setArticles] = useState([]);
-  console.log(articles);
-  const getArticles = async () => {
-    try {
-      const res = await axios.get("http://localhost:4000/");
-      setArticles(res.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getArticles();
-  }, []);
-
-  function extractSRC(htmlString) {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(htmlString, "text/html");
-
-    // Extract the src attribute value
-    const srcValue = doc.querySelector("img").getAttribute("src");
-    return srcValue;
-  }
-
   return (
-    <div className="relative bg-primary max-lg:h-full max-lg:pb-16">
+    <div className="relative bg-primary h-[35rem] max-lg:h-full max-lg:pb-16">
       <div className="flex max-lg:flex-col max-lg:gap-y-16 items-center justify-between max-w-screen-2xl max-2xl:ml-auto 2xl:mx-auto 2xl:padX max-lg:px-0 lg:pl-10 max-lg:padX">
         <div className="w-[45%] h-full max-lg:pt-16 max-lg:w-full text-gray">
           <div className="font-DMSefif text-7xl max-lg:text-6xl max-sm:text-5xl pb-5 leading-[1.1] max-sm:leading-[1.08]">
@@ -77,46 +50,13 @@ const Hero = () => {
           </div>
         </div>
         <div className="w-[50%] max-lg:w-full">
-          <Swiper
-            slidesPerView={1}
-            className="mySwiper"
-            onSlideChange={(swiper) => {
-              setIsBegin(swiper.isBeginning);
-              setIsEnd(swiper.isEnd);
-            }}
-            autoplay={{
-              delay: 4000,
-              disableOnInteraction: false,
-            }}
-            modules={[Navigation, Autoplay]}
-            onInit={() => setInit(true)}
-            navigation={{
-              prevEl: prevRef.current,
-              nextEl: nextRef.current,
-            }}
-          >
-            {articles.map((feed) => (
-              <SwiperSlide key={feed.item.title} className="">
-                <Link to={feed.item.link} target="_blank" rel="noopener noreferrer">
-                  <div className="relative h-[35rem] max-lg:h-[35rem] max-sm:h-[25rem]">
-                    <img
-                      src={extractSRC(feed.item.content)}
-                      alt="party activity"
-                      className="h-[35rem] max-lg:h-[35rem] max-sm:h-[25rem] w-full object-cover"
-                    />
-                    <div className="absolute right-0 left-0 bottom-0 bg-dark bg-opacity-80 px-3 py-6 max-sm:py-4 text-white space-y-5">
-                      <div className="bg-white bg-opacity-80 text-primary w-fit px-2 py-1 text-base max-sm:text-sm mb-4">
-                        {/* {feed.category} */}
-                      </div>
-                      <span className=" text-3xl max-md:text-2xl max-sm:text-xl font-Heebo300 max-md:font-Heebo400 tracking-normal">
-                        {feed.item.title}
-                      </span>
-                    </div>
-                  </div>
-                </Link>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+          <NewsFeeds
+            prevRef={prevRef}
+            nextRef={nextRef}
+            setInit={setInit}
+            setBegin={setBegin}
+            setEnd={setEnd}
+          />
         </div>
       </div>
     </div>
---
+++ src/ui/NewsFeeds.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import axios from "axios";
+import "swiper/css";
+import { Link } from "react-router-dom";
+
+const NewsFeeds = ({ prevRef, nextRef, setBegin, setEnd, setInit }) => {
+  const [articles, setArticles] = useState([]);
+  const getArticles = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/");
+      setArticles(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+  function extractSRC(htmlString) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlString, "text/html");
+    // Extract the src attribute value
+    const srcValue = doc.querySelector("img").getAttribute("src");
+    return srcValue;
+  }
+
+  function formatDate(date) {
+    let formatted = { day: "numeric", month: "long", year: "numeric" };
+    let articleDate = new Date(date).toLocaleDateString("en-NG", formatted);
+    return articleDate;
+  }
+
+  return (
+    <div>
+      <Swiper
+        slidesPerView={1}
+        className="mySwiper"
+        onSlideChange={(swiper) => {
+          setBegin(swiper.isBeginning);
+          setEnd(swiper.isEnd);
+        }}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        modules={[Navigation, Autoplay]}
+        onInit={() => setInit(true)}
+        navigation={{
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
+      >
+        {articles.map((feed) => (
+          <SwiperSlide key={feed.item.title} className="">
+            <Link to={feed.item.link} target="_blank" rel="noopener noreferrer">
+              <div className="relative h-[35rem] max-lg:h-[35rem] max-md:h-[30rem] max-sm:h-[25rem]">
+                <img
+                  src={extractSRC(feed.item.content)}
+                  alt="party activity"
+                  className="h-full w-full object-cover"
+                />
+                <div className="absolute right-0 left-0 bottom-0 bg-dark bg-opacity-90 px-3 py-6 max-sm:py-4 text-white space-y-5">
+                  <div className="text-sm uppercase mb-4">
+                    {formatDate(feed.item.pubDate)}
+                  </div>
+                  <span className=" text-3xl max-md:text-2xl max-sm:text-xl font-Heebo300 max-md:font-Heebo400 tracking-normal">
+                    {feed.item.title}
+                  </span>
+                </div>
+              </div>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+export default NewsFeeds;
