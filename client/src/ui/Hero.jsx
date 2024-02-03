5fd0be4dfdf8f38b4b112fec0e51c822cf75cf32
Fortunate ogodu <58645821+iam4tunate@users.noreply.github.com>
2024-02-03 02:38:39 +0100

    finally fixed vercel deployment

--- client/src/ui/Hero.jsx
+++ client/src/ui/Hero.jsx
@@ -16,8 +16,8 @@ const Hero = () => {
 
   return (
     <div className="relative bg-primary h-[35rem] max-lg:h-full max-lg:pb-16">
-      <div className="flex max-lg:flex-col max-lg:gap-y-16 items-center justify-between max-w-screen-2xl max-2xl:ml-auto 2xl:mx-auto 2xl:padX max-lg:px-0 lg:pl-10 max-lg:padX">
-        <div className="w-[45%] h-full max-lg:pt-16 max-lg:w-full text-gray">
+      <div className="flex items-center justify-between max-lg:flex-col max-lg:gap-y-16 max-w-screen-2xl max-2xl:ml-auto 2xl:mx-auto 2xl:padX max-lg:px-0 lg:pl-10 max-lg:padX h-full">
+        <div className="w-[45%] max-lg:pt-16 max-lg:w-full text-gray">
           <div className="font-DMSefif text-7xl max-lg:text-6xl max-sm:text-5xl pb-5 leading-[1.1] max-sm:leading-[1.08]">
             Welcome to <span className="text-secondary">kálésanwá</span> Group.
           </div>
@@ -31,27 +31,27 @@ const Hero = () => {
               Become a member
             </button>
           </Link>
+          {articles.length > 0 && (
+            <div className="z-20 w-fit absolute bottom-8 max-lg:bottom-2 right-1/2 max-lg:right-4 max-lg:py-4 flex items-center gap-x-3 2xl:mr-4">
+              <div
+                ref={prevRef}
+                className={`${
+                  isBegin && "opacity-30"
+                } opacity- border border-gray h-8 w-8 rounded-full flex items-center justify-center cursor-pointer shadow`}
+              >
+                <FaArrowLeft className="text-gray text-lg" />
+              </div>
+              <div
+                ref={nextRef}
+                className={`${
+                  isEnd && "opacity-30"
+                } opacity- border border-gray h-8 w-8 rounded-full flex items-center justify-center cursor-pointer shadow`}
+              >
+                <FaArrowRight className="text-gray text-lg" />
+              </div>
+            </div>
+          )}
         </div>
-        {articles.length && (
-          <div className="z-20 w-fit absolute bottom-8 max-lg:bottom-2 right-1/2 max-lg:right-4 max-lg:py-4 flex items-center gap-x-3 2xl:mr-4">
-            <div
-              ref={prevRef}
-              className={`${
-                isBegin && "opacity-30"
-              } opacity- border border-gray h-8 w-8 rounded-full flex items-center justify-center cursor-pointer shadow`}
-            >
-              <FaArrowLeft className="text-gray text-lg" />
-            </div>
-            <div
-              ref={nextRef}
-              className={`${
-                isEnd && "opacity-30"
-              } opacity- border border-gray h-8 w-8 rounded-full flex items-center justify-center cursor-pointer shadow`}
-            >
-              <FaArrowRight className="text-gray text-lg" />
-            </div>
-          </div>
-        )}
         <div className="w-[50%] max-lg:w-full">
           <NewsFeeds
             prevRef={prevRef}
