5d6d6d604c7ced4446a6e39dec1963679def1efe
Fortunate ogodu <58645821+iam4tunate@users.noreply.github.com>
2024-02-03 02:30:00 +0100

    finally fixed vercel deployment

--- client/src/ui/NewsFeeds.jsx
+++ client/src/ui/NewsFeeds.jsx
@@ -5,19 +5,19 @@ import axios from "axios";
 import "swiper/css";
 import { Link } from "react-router-dom";
 
-const NewsFeeds = ({ prevRef, nextRef, setBegin, setEnd, setInit }) => {
-  const [articles, setArticles] = useState([]);
-
+const NewsFeeds = ({
+  prevRef,
+  nextRef,
+  setBegin,
+  setEnd,
+  setInit,
+  articles,
+  setArticles,
+}) => {
   useEffect(() => {
     const getArticles = async () => {
       try {
-        const res = await axios.get("https://kalesanwa-server.vercel.app/", {
-          method: "GET",
-          mode: "no-cors",
-          headers: {
-            "Content-Type": "application/json", // Include any headers needed for your request
-          },
-        });
+        const res = await axios.get("https://kalesanwa-server.vercel.app/");
         setArticles(res.data);
       } catch (error) {
         return <div className="text-white text-4xl">{error.message}</div>;
@@ -41,48 +41,46 @@ const NewsFeeds = ({ prevRef, nextRef, setBegin, setEnd, setInit }) => {
   }
 
   return (
-    <div>
-      <Swiper
-        slidesPerView={1}
-        className="mySwiper"
-        onSlideChange={(swiper) => {
-          setBegin(swiper.isBeginning);
-          setEnd(swiper.isEnd);
-        }}
-        autoplay={{
-          delay: 4000,
-          disableOnInteraction: false,
-        }}
-        modules={[Navigation, Autoplay]}
-        onInit={() => setInit(true)}
-        navigation={{
-          prevEl: prevRef.current,
-          nextEl: nextRef.current,
-        }}
-      >
-        {articles.map((feed) => (
-          <SwiperSlide key={feed.item.title} className="">
-            <Link to={feed.item.link} target="_blank" rel="noopener noreferrer">
-              <div className="relative h-[35rem] max-lg:h-[35rem] max-md:h-[30rem] max-sm:h-[25rem]">
-                <img
-                  src={extractSRC(feed.item.content)}
-                  alt="party activity"
-                  className="h-full w-full object-cover"
-                />
-                <div className="absolute right-0 left-0 bottom-0 bg-dark bg-opacity-90 px-3 py-6 max-sm:py-4 text-gray opacity-90 space-y-5">
-                  <div className="text-sm uppercase mb-4">
-                    {formatDate(feed.item.pubDate)}
-                  </div>
-                  <span className=" text-3xl max-md:text-2xl max-sm:text-xl font-Heebo300 max-md:font-Heebo400 tracking-normal">
-                    {feed.item.title}
-                  </span>
+    <Swiper
+      slidesPerView={1}
+      className="mySwiper"
+      onSlideChange={(swiper) => {
+        setBegin(swiper.isBeginning);
+        setEnd(swiper.isEnd);
+      }}
+      autoplay={{
+        delay: 4000,
+        disableOnInteraction: false,
+      }}
+      modules={[Navigation, Autoplay]}
+      onInit={() => setInit(true)}
+      navigation={{
+        prevEl: prevRef.current,
+        nextEl: nextRef.current,
+      }}
+    >
+      {articles.map((feed) => (
+        <SwiperSlide key={feed.item.title} className="w-full">
+          <Link to={feed.item.link} target="_blank" rel="noopener noreferrer">
+            <div className="relative h-[35rem] max-lg:h-[35rem] max-md:h-[30rem] max-sm:h-[25rem]">
+              <img
+                src={extractSRC(feed.item.content)}
+                alt="party activity"
+                className="h-full w-full object-cover"
+              />
+              <div className="absolute right-0 left-0 bottom-0 bg-dark bg-opacity-90 px-3 py-6 max-sm:py-4 text-gray opacity-90 space-y-5">
+                <div className="text-sm uppercase mb-4">
+                  {formatDate(feed.item.pubDate)}
                 </div>
+                <span className=" text-3xl max-md:text-2xl max-sm:text-xl font-Heebo300 max-md:font-Heebo400 tracking-normal">
+                  {feed.item.title}
+                </span>
               </div>
-            </Link>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+            </div>
+          </Link>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 export default NewsFeeds;
